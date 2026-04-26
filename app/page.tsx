822977130e1dd5de8ab1bd38ba1466bfdc4cf79f
sarah-kami <sarakamayestani2004@gmail.com>
2026-04-26 11:06:08 +0330

    start project

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="p-10">
+      <h1 className="text-3xl font-bold">
+        فروشگاه محصولات پوستی
+      </h1>
+    </main>
+  );
+}
